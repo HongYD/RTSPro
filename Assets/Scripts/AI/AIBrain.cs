0abf1d29a33763ad6d770256ec7a65cb7845d1cd
Dennis <nirvanagun93@gmail.com>
2024-02-27 16:17:43 +0800

    update ai brain - scoring

--- Assets/Scripts/AI/AIBrain.cs
+++ Assets/Scripts/AI/AIBrain.cs
@@ -27,17 +27,46 @@ namespace RTSPro.Core
         /// <param name="actionsAvailable"></param>
         private void DecideBestAction(AIAction[] actionsAvailable)
         {
-            
+            float score = 0f;
+            int nextBestActionIndex = 0;
+            for(int i = 0; i < actionsAvailable.Length; i++)
+            {
+                if (ScoreAction(actionsAvailable[i]) > score)
+                {
+                    nextBestActionIndex = i;
+                    score = actionsAvailable[i].Score;
+                }
+            }
+            bestAction = actionsAvailable[nextBestActionIndex];
+
         }
 
         /// <summary>
         /// loop through all the considerations of the action
         /// score all the considerations
+        /// average all the consideration scores ==> overall action score
         /// </summary>
         /// <param name="aiAction"></param>
-        private void ScoreAction(AIAction aiAction)
+        private float ScoreAction(AIAction aiAction)
         {
+            float score = 1f;
+            for(int i = 0; i<aiAction.considerations.Length; i++)
+            {
+                float considerationScore = aiAction.considerations[i].ScoreConsideration();
+                score *= considerationScore;
+                if(score == 0)
+                {
+                    aiAction.Score = 0;
+                    return aiAction.Score;
+                }
+            }
+            //Averaing scheme of overall score
+            float originalScore = score;
+            float modFactor = 1 - (1 / aiAction.considerations.Length);//Length越长，modFactor越大
+            float makeupValue = (1 - originalScore) * modFactor;//originalScore越大,makeupValue越小
+            aiAction.Score = originalScore + (makeupValue * originalScore);
 
+            return aiAction.Score;
         }
     }
 }
